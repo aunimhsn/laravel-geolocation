1585c2c06586088e01b2b8aaddb5d00b0fbd57c1
Aunim HASSAN <aunim.hassan@gmail.com>
2021-05-01 23:29:20 +0200

    feat: tailwindcss setup

--- geolocation/resources/views/home/index.blade.php
+++ geolocation/resources/views/home/index.blade.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<x-layouts.master>
+    <div class="bg-blue-600 text-white p-4">
+        Hello World!
+    </div>
+</x-layouts.master>
