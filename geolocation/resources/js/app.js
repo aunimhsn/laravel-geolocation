0a90f7a848939039fb89516e78ee2fae9f8b7468
Aunim HASSAN <aunim.hassan@gmail.com>
2021-05-03 21:54:42 +0200

    feat: map

--- geolocation/resources/js/app.js
+++ geolocation/resources/js/app.js
@@ -1,1 +1,9 @@
 require('./bootstrap');
+
+var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+ 
+mapboxgl.accessToken = 'pk.eyJ1IjoiYXVuaW1oc24iLCJhIjoiY2tuazlxbG8wMDZwMzJvcGVucnhqdDhxZCJ9.FghoO5lb2VZkbxgN3cSzSg';
+var map = new mapboxgl.Map({
+    container: 'mapbox',
+    style: 'mapbox://styles/mapbox/streets-v11'
+});
--- geolocation/resources/views/home/index.blade.php
+++ geolocation/resources/views/home/index.blade.php
@@ -2,4 +2,7 @@
     <div class="bg-blue-600 text-white p-4">
         Tracking!
     </div>
+
+    <!-- mapbox -->
+    <div id="mapbox"></div>
 </x-layouts.master>
