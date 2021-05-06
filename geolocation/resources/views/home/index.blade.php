94f00c21ca9582c3f7fc942b17e93a3cfc463652
Aunim HASSAN <aunim.hassan@gmail.com>
2021-05-06 14:18:29 +0200

    feat: cta button

--- geolocation/resources/views/home/index.blade.php
+++ geolocation/resources/views/home/index.blade.php
@@ -5,4 +5,7 @@
 
     <!-- mapbox -->
     <div id="mapbox" height="100"></div>
+
+    <!-- CTA -->
+    <button id="pull" class="px-4 py-2 bg-blue-600 hover:bg-blue-800 text-white">Pull locations</button>
 </x-layouts.master>
